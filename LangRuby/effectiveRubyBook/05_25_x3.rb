48f89263ef55be33e944f12280a3db06bcaf8aa5
Sujeet Sukumaran <sujeet.kumar@gmail.com>
2017-02-03 17:22:10 -0700

    finished with lessons

--- LangRuby/effectiveRubyBook/05_25_x3.rb
+++ LangRuby/effectiveRubyBook/05_25_x3.rb
@@ -7,3 +7,16 @@ Use File.join to build file paths safely
 
 =end
 
+path_fragments = ["/tmp", "#{rand}", "hello.txt"]
+
+filename = File.join(path_fragments)
+p filename
+directory = File.dirname(filename)
+
+p directory
+
+Dir.mkdir(directory)
+
+File.open(filename, "w") do |fp|
+  fp.write("hello world")
+end
